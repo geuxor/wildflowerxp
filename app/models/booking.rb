c2a0ed51d1aaa3c8ade0aa0d8a6c99ea79394dbd
German B <geuxor@dr.com>
2021-03-09 12:04:48 +0100

    created models, validation, a few seeds, association, routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,20 @@
 class Booking < ApplicationRecord
+  before_create :set_total_price
   belongs_to :user
   belongs_to :experience
+  has_many :reviews
+  validates :nr_of_people, numericality: true
+  validates :nr_of_people, :start_date, :end_date, presence: true
+  enum status: [:pending, :accepted, :declined]
+
+  private
+
+  def duration
+    (end_date - start_date).to_i + 1
+  end
+
+  def set_total_price
+    self.total_price = experience.price * nr_of_people * duration
+  end
+  
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,9 @@
 class Experience < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :reviews, through: :bookings
+  validates :title, :description, :location, :price, :activity, :max_guests, :meeting_point, :policies, :activity_effort, presence: true
+  validates :price, :max_guests, numericality: true
+  validates :activity, inclusion: { in: %w( Sport Art Nature Music Food Wild ) }
+  validates :activity_effort, inclusion: { in: 1..5 }
 end
