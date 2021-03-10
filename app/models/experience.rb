51af3f4f7563b250a345d5f80b313ba20d695e8f
German B <geuxor@dr.com>
2021-03-10 15:14:52 +0100

    updated new xp form style and added algolia and styled form cats

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,14 +1,15 @@
 class Experience < ApplicationRecord
+  DEFAULT_IMG = 'https://res.cloudinary.com/geuxor/image/upload/v1615381828/samples/sheep-closeup-eating-grass_bbalee.jpg'
   ACTIVITIES = %w( Sport Art Nature Music Food Wild )
   ACTIVITY_EFFORTS = (1..5)
   has_one_attached :photo
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
-
+  
   validates :title, :description, :location, :price, :activity, :max_guests, :meeting_point, :policies, :activity_effort, presence: true
   validates :price, :max_guests, numericality: true
   validates :activity, inclusion: { in: ACTIVITIES }
   validates :activity_effort, inclusion: { in: ACTIVITY_EFFORTS }
+  
 end
-
